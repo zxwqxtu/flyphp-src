679d96d901b17cbac3e72b4ea0ebf295c611c6ab
wangqiang <wangqiang@e.hunantv.com>
2016-07-10 00:31:04 +0800

    add model function

--- src/Model/Base.php
+++ src/Model/Base.php
@@ -57,4 +57,162 @@ abstract class Base
 
         $this->db = $dbClass::getInstance()->connect($config, $this->dbType);
     }
+
+    /**
+     * 根据ID获取一条记录
+     *
+     * @param int $id id
+     *
+     * @return array
+     */
+    public function find($id)
+    {
+        return $this->findOneBy(['id' => intval($id)]);
+    }
+    /**
+     * 返回所有记录
+     *
+     * @return array|cursor
+     */
+    public function findAll()
+    {
+        return $this->findBy();
+    }
+    /**
+     * 返回符合条件的所有记录
+     *
+     * @param array $where []
+     *
+     * @return array|cursor
+     */
+    public function findBy($where=[])
+    {
+        switch ($this->dbType) {
+        case 'mongodb':
+            return $this->db->selectCollection($this->collection)->find($where);
+        default:
+            $where = $this->buildWhere($where);
+            $stmt = $this->db->prepare("SELECT * FROM {$this->table} {$where}");
+            $stmt->execute();
+            return $stmt->fetchAll();
+        }
+    }
+    /**
+     * 返回符合条件的第一条记录
+     *
+     * @param array $where []
+     *
+     * @return array|cursor
+     */
+    public function findOneBy($where=[])
+    {
+        switch ($this->dbType) {
+        case 'mongodb':
+            return $this->db->selectCollection($this->collection)->findOne($where);
+        default:
+            $where = $this->buildWhere($where);
+            $stmt = $this->db->prepare("SELECT * FROM {$this->table} {$where}");
+            $stmt->execute();
+            return $stmt->fetch();
+        }
+    }
+    /**
+     * 插入一条记录
+     *
+     * @param array $data []
+     *
+     * @return bool
+     */
+    public function insert($data=[])
+    {
+        switch ($this->dbType) {
+        case 'mongodb':
+            return $this->db->selectCollection($this->collection)->insert($data);
+        default:
+            $columns = implode(',', array_keys($data));
+            $exp = trim(str_repeat('?,', count($data)), ',');
+
+            $stmt = $this->db->prepare("INSERT INTO {$this->table}({$columns}) VALUE({$exp})");
+            return $stmt->execute(array_values($data));
+        }
+    }
+    /**
+     * 更新记录
+     *
+     * @param array $data  []
+     * @param array $where []
+     *
+     * @return int
+     */
+    public function update($data=[], $where=[])
+    {
+        switch ($this->dbType) {
+        case 'mongodb':
+            return $this->db->selectCollection($this->collection)->update(
+                $where, 
+                ['$set' => $data], 
+                ['multiple' => true, 'upsert' => false]
+            );
+        default:
+            $where = $this->buildWhere($where);
+            $value = '';
+            foreach ($data as $k => $v) {
+                $value .= "{$k}={$v},";
+            }
+            $value = trim($value, ',');
+            return $this->db->exec("UPDATE {$this->table} SET {$value} {$where}");
+        }
+    }
+    /**
+     * 删除记录
+     *
+     * @param array $where []
+     *
+     * @return int 
+     */
+    public function delete($where=[])
+    {
+        switch ($this->dbType) {
+        case 'mongodb':
+            return $this->db->selectCollection($this->collection)->remove(
+                $where, 
+                ['justOne' => false]
+            );
+        default:
+            $where = $this->buildWhere($where);
+            return $this->db->exec("DELETE FROM {$this->table} {$where}");
+        }
+    }
+    /**
+     * @example ['name' => ['%wq%', 'like'], 'id' => 1] 
+     * pdo 专属
+     *
+     * @param array $filters []
+     *
+     * @return string
+     */
+    protected function buildWhere($filters)
+    {
+        $where = 'WHERE 1 = 1 ';
+        foreach ($filters as $v) {
+            $column = $v[0];
+            $val = $v[1];
+            if (!empty($v[2])) {
+                $expression = $v[2];
+            } elseif (is_array($val)) {
+                $expression = 'in';
+            } else {
+                $expression = '=';
+            }
+
+            if (strtolower($expression) == 'in') {
+                $val = "'".implode("','", $val)."'";
+                $where .= " AND {$column} {$expression} ({$val})";
+            } else {
+                $where .= " AND {$column} {$expression} ".$this->db->quote($val);
+            }
+        } 
+        return $where;
+    }
+
 }
